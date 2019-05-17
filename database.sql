eb75dc37401953a9548b076b4c2cdab5a33fbe48
XioSC <albastroiudragos@gmail.com>
2019-05-17 16:20:01 +0300

    Schimbari MS

--- database.sql
+++ database.sql
@@ -6,7 +6,24 @@ GRANT ALL PRIVILEGES ON workhorse.* TO 'workhorse'@'localhost';
 
 USE workhorse;
 
-CREATE TABLE Users (Id INT, Email VARCHAR(100), Password VARCHAR(32));
+CREATE TABLE Users (Id INT NOT NULL AUTO_INCREMENT, Email VARCHAR(100), Password VARCHAR(32), PRIMARY KEY (Id));
+
+CREATE TABLE VirtualMachines  (Id INT NOT NULL AUTO_INCREMENT,
+                              IdUser INT NOT NULL, 
+                              Name VARCHAR(100) NOT NULL,
+                              Path VARCHAR(100) NOT NULL,
+                              Subdomain VARCHAR(100) NOT NULL,                              
+                              StartDate DATE NOT NULL,
+                              Active TINYINT NOT NULL,
+                              PRIMARY KEY (Id)
+                              );
+
+CREATE TABLE AccountBalance (
+                            Id INT NOT NULL AUTO_INCREMENT,
+                            IdUser INT NOT NULL,
+                            Balance INT NOT NULL,
+                            PRIMARY KEY(Id)
+                            )
 
 SET password
 FOR 'workhorse'@'localhost' = password('workhorsepassword')
---
+++ site/dashboard/index.php
@@ -0,0 +1,89 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="Online VPS Service">
+        <meta name="author" content="workhorse.">
+        <title>workhorse. - Dashboard</title>
+        <!-- Bootstrap core CSS -->
+        <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Custom fonts for this template -->
+        <link href="/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+        <link href="/vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
+        <link href="/https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+        <!-- Custom styles for this template -->
+        <link href="/css/landing-page.min.css" rel="stylesheet">
+        <link href="/css/site.css" rel="stylesheet">
+    </head>
+    <body>
+        <!-- Navigation -->
+        <nav class="navbar navbar-light bg-light static-top">
+        <div class="container">
+          <a class="navbar-brand" href="#">
+            <img src="/img/logo1.png" id="logo-wh"/>
+          </a>
+          <a class="btn btn-primary" href="/account/signout.php">Sign out</a>
+        </div>
+      </nav>
+
+        <!-- Masthead -->
+  <header class="masthead text-white text-center">
+        <div class="overlay"></div>
+        <div class="container">
+          <div class="row">
+            <div class="col-xl-9 mx-auto">
+              <h1 class="mb-5">workhorse.</h1>
+            </div>
+          </div>
+        </div>
+      </header>
+        
+      <!-- Icons Grid -->
+      <section class="features-icons bg-light text-center">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                <div class="features-icons-icon d-flex">
+                  <i class="icon-user m-auto text-primary"></i>
+                </div>
+                <h3>Hello, user</h3>
+                <p class="lead mb-0">Here are your virtual machines:</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="container" id="virtual-machines">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                    <div class="features-icons-icon d-flex">
+                        <i class="icon-screen-desktop m-auto text-primary"></i>
+                    </div>
+                    <h3>Ubuntu 18.04</h3>
+                    <p class="lead mb-0">ssh user@wrecktheline.com:2222</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </section>
+
+          <!-- Footer -->
+        <footer class="footer bg-light">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12 h-100 text-center text-lg-center my-auto">
+              <p class="text-muted small mb-4 mb-lg-0">&copy; workhorse. 2019. All Rights Reserved.</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    
+      <!-- Bootstrap core JavaScript -->
+      <script src="/vendor/jquery/jquery.min.js"></script>
+      <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    </body>
+
+</html>
